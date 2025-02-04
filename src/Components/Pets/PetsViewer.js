562e4e09d86f67442879a0b3c394a98e7c45e482
Chowiya <nd1553410@gmail.com>
2025-02-04 10:51:23 +0530

    updated petviewer.js

--- src/Components/Pets/PetsViewer.js
+++ src/Components/Pets/PetsViewer.js
@@ -5,26 +5,26 @@ import { formatDistanceToNow } from 'date-fns';
 const PetsViewer = (props) => {
   const [showPopup, setShowPopup] = useState(false);
 
-  const togglePopup = () => {
-    setShowPopup(!showPopup);
-  };
+  const togglePopup = () => setShowPopup((prev) => !prev);
+  const closePopup = () => setShowPopup(false);
 
   const formatTimeAgo = (updatedAt) => {
     const date = new Date(updatedAt);
     return formatDistanceToNow(date, { addSuffix: true });
   };
 
-
   console.log("API Base URL:", process.env.REACT_APP_URL);
   console.log("Full Image URL:", `${process.env.REACT_APP_URL}/images/${props.pet.filename}`);
   console.log("Pet Data:", props.pet);
+  console.log("Popup state:", showPopup);
 
   return (
     <div className='pet-view-card'>
       <div className='pet-card-pic'>
-
-        <img src={`https://home4paw-be-1.onrender.com/images/${props.pet.filename  || 'default-image.jpg'}`} alt={props.pet.name}/>
-       
+        <img
+          src={`https://home4paw-be-1.onrender.com/images/${props.pet.filename || 'default-image.jpg'}`}
+          alt={props.pet.name}
+        />
       </div>
       <div className='pet-card-details'>
         <h2>{props.pet.name}</h2>
@@ -33,18 +33,18 @@ const PetsViewer = (props) => {
         <p><b>Size:</b> {props.pet.size}</p>
         <p><b>Location:</b> {props.pet.area}</p>
         <p><b>Medical History:</b> {props.pet.medicalHistory}</p>
-        <p><b>Breed:</b>{props.pet.breed}</p>
+        <p><b>Breed:</b> {props.pet.breed}</p>
         <p>{formatTimeAgo(props.pet.updatedAt)}</p>
       </div>
       <div className='show-interest-btn'>
         <button onClick={togglePopup}>Show Interest <i className="fa fa-paw"></i></button>
       </div>
-      {showPopup && (
+      {showPopup && props.pet && (
         <div className='popup'>
           <div className='popup-content'>
-            <AdoptForm closeForm={togglePopup} pet={props.pet}/>
+            <AdoptForm closeForm={closePopup} pet={props.pet} />
           </div>
-          <button onClick={togglePopup} className='close-btn'>
+          <button onClick={closePopup} className='close-btn'>
             Close <i className="fa fa-times"></i>
           </button>
         </div>
@@ -54,4 +54,5 @@ const PetsViewer = (props) => {
 };
 
 export default PetsViewer;
+
 
